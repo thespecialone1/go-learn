5ad97fbeb49c1d1ea2a4cb0c07556e7c53500399
thespecialone1 <s3neo36@gmail.com>
2024-09-15 17:53:09 +0400

    hello-test

---
+++ helloWorld-Test/hello_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	got := Hello("chris")
+	want := "Hello, chris"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
